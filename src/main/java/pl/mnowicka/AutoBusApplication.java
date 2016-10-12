e6047a006b9bf77f20210a0622c43a3cf5c14f1e
Magda <mnowickam@gmail.com>
2016-10-12 10:18:42 +0200

    Pierwszy commit

---
+++ src/main/java/pl/mnowicka/AutoBusApplication.java
@@ -0,0 +1,12 @@
+package pl.mnowicka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoBusApplication.class, args);
+	}
+}
